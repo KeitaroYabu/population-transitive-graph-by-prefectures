33badca182c5fde8c3d40290ba226d039ddbdc06
KeitaroYabu <y2k.prog@gmail.com>
2023-08-12 10:04:38 +0000

    checkboxの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Checkbox } from "./components/checkbox";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">都道府県別総人口推移グラフ</h1>
+      <div className="flex flex-row text-center">
+        <Checkbox isChecked={true} />
+        北海道
+      </div>
+      <Checkbox isChecked={false} />
     </main>
   );
 }
