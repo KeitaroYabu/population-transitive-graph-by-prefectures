5ca5cb4bc10d58085a06b7a83ee004954663873f
KeitaroYabu <y2k.prog@gmail.com>
2023-08-14 09:50:54 +0000

    人口構成を取得

--- src/app/config.ts
+++ src/app/config.ts
@@ -10,10 +10,15 @@ if (!process.env.RESAS_PREFECTURES_PATH) {
   throw new Error("環境変数RESAS_PREFECTURES_PATHが設定されていません。");
 }
 
+if (!process.env.RESAS_POPULATION_PATH) {
+  throw new Error("環境変数RESAS_POPULATION_PATHが設定されていません。");
+}
+
 export const appConfig = {
   resas: {
     apiKey: process.env.RESAS_API_KEY,
     hostDomain: process.env.RESAS_HOST_DOMAIN,
     prefecturesPath: process.env.RESAS_PREFECTURES_PATH,
+    populationPath: process.env.RESAS_POPULATION_PATH,
   },
 };
